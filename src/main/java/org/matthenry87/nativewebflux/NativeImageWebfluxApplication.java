fdf55ac523a53bb764d250896a12d98cfde1329e
Matt Henry <Matt.Henry@fisglobal.com>
2022-12-30 09:30:47 -0700

    Initial commit

---
+++ src/main/java/org/matthenry87/nativewebflux/NativeImageWebfluxApplication.java
@@ -0,0 +1,14 @@
+package org.matthenry87.nativewebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeImageWebfluxApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(NativeImageWebfluxApplication.class, args);
+    }
+
+}
